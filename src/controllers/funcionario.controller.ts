e33ec435223ecf2e0760c3d5726227b42ea407aa
Gabriel Cruz <gabriel.cp.moraes@gmail.com>
2026-06-02 19:40:20 -0300

    Requisição HTTP e chama service para funcionario

---
+++ src/controllers/funcionario.controller.ts
@@ -0,0 +1,48 @@
+import { Request, Response, NextFunction } from 'express';
+import { funcionarioService } from '../services/funcionario.service';
+import { createFuncionarioSchema, updateFuncionarioSchema } from '../schemas/funcionario.schema';
+
+export const funcionarioController = {
+  async list(_req: Request, res: Response, next: NextFunction) {
+    try {
+      res.json(await funcionarioService.list());
+    } catch (err) {
+      next(err);
+    }
+  },
+
+  async getById(req: Request, res: Response, next: NextFunction) {
+    try {
+      res.json(await funcionarioService.getById(Number(req.params.id)));
+    } catch (err) {
+      next(err);
+    }
+  },
+
+  async create(req: Request, res: Response, next: NextFunction) {
+    try {
+      const data = createFuncionarioSchema.parse(req.body);
+      res.status(201).json(await funcionarioService.create(data));
+    } catch (err) {
+      next(err);
+    }
+  },
+
+  async update(req: Request, res: Response, next: NextFunction) {
+    try {
+      const data = updateFuncionarioSchema.parse(req.body);
+      res.json(await funcionarioService.update(Number(req.params.id), data));
+    } catch (err) {
+      next(err);
+    }
+  },
+
+  async remove(req: Request, res: Response, next: NextFunction) {
+    try {
+      await funcionarioService.remove(Number(req.params.id));
+      res.status(204).send();
+    } catch (err) {
+      next(err);
+    }
+  },
+};
