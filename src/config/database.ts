724bd78731f2551c351105b5c8d58850543ef5d4
Vitao-campos <vaffonsocampos@gmail.com>
2026-06-03 22:46:49 -0300

    Cria repository de turma e database.ts

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,1 +1,12 @@
+import sqlite3 from 'sqlite3';
+import path from 'path';
+
+const sqlite = sqlite3.verbose();
+const dbPath = path.join(process.cwd(), 'src', 'database', 'database.db');
+
+export const db = new sqlite.Database(dbPath, (err) => {
+	if (err) {
+		console.error('Erro ao conectar ao banco de dados:', err);
+	}
+});
 
