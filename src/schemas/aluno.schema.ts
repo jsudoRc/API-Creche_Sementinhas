253afce31c758dcaca894e18e6c3983bce53b1aa
Ezequiel Buzi Rodrigues <ezequielrodrigues1996@gmail.com>
2026-06-07 18:51:41 -0300

    atualiza o arquivo zod com o cpf e mudanã na variavel sexo

--- src/schemas/aluno.schema.ts
+++ src/schemas/aluno.schema.ts
@@ -3,11 +3,10 @@ import { z } from 'zod';
 export const createAlunoSchema = z.object({
     nome: z.string().min(2, 'O nome é obrigatório.'),
     data_nasc: z.string(),
+    cpf: z.string().min(11, 'O CPF deve conter 11 caracteres.').max(11, 'O CPF deve conter 11 caracteres.'),
     andarilha: z.number().int().min(0).max(1),
-    autorizacao_img: z.number().int().min(0).max(1),
-    
-    // A CORREÇÃO ESTÁ AQUI: Trocamos o .optional() por .nullable().default(null)
-    sexo: z.string().nullable().default(null),
+    autorizacao_img: z.string().nullable().default(null),
+    sexo: z.enum(['Masculino', 'Feminino']),
     receita_antitermico: z.string().nullable().default(null),
     cirurgia_qual: z.string().nullable().default(null),
     cirurgia_tempo: z.string().nullable().default(null),
