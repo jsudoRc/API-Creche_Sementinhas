19bd788e67711accf3c65219ef0613ce2ef1d9f4
Ezequiel Buzi Rodrigues <ezequielrodrigues1996@gmail.com>
2026-06-07 18:57:33 -0300

    atualiza o services com as funções para usar imagem no banco de dados

--- src/services/aluno.services.ts
+++ src/services/aluno.services.ts
@@ -1,4 +1,4 @@
-
+import fs from 'fs';
 import { turmaRepository } from '../repositories/turma.repository';
 import { alunoRepository } from '../repositories/aluno.repository';
 import { funcionarioRepository } from '../repositories/funcionario.repository';
@@ -17,14 +17,9 @@ export const alunoService = {
   },
 
   async create(input: CreateAlunoInput) {
+    const turma = await turmaRepository.findById(input.turma_id);
+    if (!turma) throw new AppError('A turma informada não existe no sistema!', 404);
 
-
-    // Futuramente, podemos adicionar uma checagem aqui para ver se a turma_id informada existe
-    // criado XD 
-    const turma = await turmaRepository.findById(input.turma_id)
-    if(!turma)throw new AppError('A turma informada não existe no sistema!',404);
-
-    // 2. Verifica se o funcionário existe
     const funcionario = await funcionarioRepository.findById(input.funcionario_id);
     if (!funcionario) throw new AppError('O funcionário responsável pelo cadastro não existe.', 404);
 
@@ -32,24 +27,32 @@ export const alunoService = {
   },
 
   async update(id: number, input: UpdateAlunoInput) {
-    await this.getById(id); // Garante que o aluno existe
+    await this.getById(id);
 
-    // Se estiverem tentando trocar o aluno de turma, verifica se a turma nova existe
     if (input.turma_id) {
-        const turma = await turmaRepository.findById(input.turma_id);
-        if (!turma) throw new AppError('A nova turma informada não existe.', 404);
+      const turma = await turmaRepository.findById(input.turma_id);
+      if (!turma) throw new AppError('A nova turma informada não existe.', 404);
     }
 
-    // Se estiverem alterando quem fez o cadastro
     if (input.funcionario_id) {
-        const funcionario = await funcionarioRepository.findById(input.funcionario_id);
-        if (!funcionario) throw new AppError('O funcionário informado não existe.', 404);
+      const funcionario = await funcionarioRepository.findById(input.funcionario_id);
+      if (!funcionario) throw new AppError('O funcionário informado não existe.', 404);
     }
+
     return alunoRepository.update(id, input);
   },
 
   async remove(id: number) {
-    await this.getById(id);
+    const aluno = await this.getById(id);
+
+    // Deleta os arquivos físicos se existirem
+    if (aluno.autorizacao_img) {
+      fs.unlink(aluno.autorizacao_img, () => {});
+    }
+    if (aluno.receita_antitermico) {
+      fs.unlink(aluno.receita_antitermico, () => {});
+    }
+
     await alunoRepository.delete(id);
   },
 };
