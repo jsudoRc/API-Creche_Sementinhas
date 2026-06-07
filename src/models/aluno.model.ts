7d2e570bb675fde15d545c83cb1d42e4a139dfbc
Ezequiel Buzi Rodrigues <ezequielrodrigues1996@gmail.com>
2026-06-07 18:50:01 -0300

    modifica o aluno.models de acordo com o schqma.sql

--- src/models/aluno.model.ts
+++ src/models/aluno.model.ts
@@ -2,14 +2,15 @@ export interface Aluno {
     id:number;
     nome:string;
     data_nasc:string;
+    cpf: string;
     andarilha:number;
-    autorizacao_img:number;
-    sexo:string |null;
+    autorizacao_img:string | null;
+    sexo: 'Masculino' | 'Feminino';
     receita_antitermico:string|null;
     cirurgia_qual:string |null;
     cirurgia_tempo:string |null;
     observacoes: string | null;
-
+    
     turma_id:number;
     funcionario_id:number;
 
