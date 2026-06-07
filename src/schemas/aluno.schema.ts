f70d11214359f8bdfc9889cf52b1e3ef07e7ddbf
Ezequiel Buzi Rodrigues <ezequielrodrigues1996@gmail.com>
2026-06-07 20:41:00 -0300

    correção de bug ao alterar o cadastro via put, que retirava o endereçamento de imagem

--- src/schemas/aluno.schema.ts
+++ src/schemas/aluno.schema.ts
@@ -11,9 +11,21 @@ export const createAlunoSchema = z.object({
     cirurgia_qual: z.string().nullable().default(null),
     cirurgia_tempo: z.string().nullable().default(null),
     observacoes: z.string().nullable().default(null),
-    
     turma_id: z.number().int().positive(),
     funcionario_id: z.number().int().positive(),
 });
 
-export const updateAlunoSchema = createAlunoSchema.partial();
+export const updateAlunoSchema = z.object({
+    nome: z.string().min(2).optional(),
+    data_nasc: z.string().optional(),
+    cpf: z.string().min(11).max(11).optional(),
+    andarilha: z.number().int().min(0).max(1).optional(),
+    autorizacao_img: z.string().nullable().optional(), // sem .default(null)
+    sexo: z.enum(['Masculino', 'Feminino']).optional(),
+    receita_antitermico: z.string().nullable().optional(), // sem .default(null)
+    cirurgia_qual: z.string().nullable().optional(),
+    cirurgia_tempo: z.string().nullable().optional(),
+    observacoes: z.string().nullable().optional(),
+    turma_id: z.number().int().positive().optional(),
+    funcionario_id: z.number().int().positive().optional(),
+});
