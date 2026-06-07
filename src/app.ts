e8fc1011c036572ba46a2b7c355d8f59b1d3ade6
Julio <julio.src2006@gmail.com>
2026-06-07 16:32:35 -0300

    Configura o express

---
+++ src/app.ts
@@ -0,0 +1,17 @@
+import  express  from "express";
+import  routes from "./routes";
+import cors from 'cors'
+import { errorHandler } from "./middlewares/error-handler";
+
+export const app = express()
+
+// Permite que o Express entenda JSON no corpo das requisições (req.body)
+app.use(cors());//2. Libera a catraca para o Front-end entrar com requisições
+app.use(express.json());
+
+
+// Pluga todas as rotas
+app.use(routes);
+
+// O middleware de erro DEVE ser sempre o último 'app.use'
+app.use(errorHandler);
