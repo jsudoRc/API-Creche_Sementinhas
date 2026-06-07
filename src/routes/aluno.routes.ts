df7739777791fa331dec8c4b1eee6b78d754a192
Ezequiel Buzi Rodrigues <ezequielrodrigues1996@gmail.com>
2026-06-07 19:00:35 -0300

    atualiza o routes para usar imagens no banco

--- src/routes/aluno.routes.ts
+++ src/routes/aluno.routes.ts
@@ -1,12 +1,19 @@
 import { Router } from 'express';
 import { alunoController } from '../controllers/aluno.controller';
+import { upload } from '../config/multer';
 
 const router = Router();
 
 router.get('/', alunoController.list);
 router.get('/:id', alunoController.getById);
-router.post('/', alunoController.create);
-router.put('/:id', alunoController.update);
+router.post('/', upload.fields([
+    { name: 'autorizacao_img', maxCount: 1 },
+    { name: 'receita_antitermico', maxCount: 1 }
+]), alunoController.create);
+router.put('/:id', upload.fields([
+    { name: 'autorizacao_img', maxCount: 1 },
+    { name: 'receita_antitermico', maxCount: 1 }
+]), alunoController.update);
 router.delete('/:id', alunoController.remove);
 
 export default router;
