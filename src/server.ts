9f360af1329d48b50aed952d93ab08344ddab21b
Julio <julio.src2006@gmail.com>
2026-06-07 23:22:47 -0300

    Adiciona config para subir server na nuvem

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,8 @@
 import { app } from './app';
 
-const PORT = 3000;
+// Do jeito certo para a nuvem:
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`Servidor rodando em http://localhost:${PORT}`);
+    console.log(`Servidor rodando na porta ${PORT}`);
 });
